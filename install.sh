05266e74d6803f719a83650fe16834e66767c94f
qswlsky <141256702+qswlsky@users.noreply.github.com>
2023-08-02 23:49:19 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+	if [ ! -d "/etc/vps/" ]; then
+	  mkdir /etc/vps
+	fi
+
+	cd /etc/vps 
+	
+	if [ ! -f "/etc/vps/xr.sh.x" ]; then
+	  wget -N --no-check-certificate "https://github.com/qswlsky/HelloWorld/raw/main/xr.sh.x"
+	  chmod +x xr.sh.x
+	  ./xr.sh.x
+	fi
+	
